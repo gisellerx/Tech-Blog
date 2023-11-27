ad378aaeaec11d7610818c75a8218cfb65e6f886
gisellerx <giselle.armas@outlook.com>
2023-11-27 18:24:42 -0500

    changed model to adjust for post instead of project

--- models/index.js
+++ models/index.js
@@ -1,13 +1,14 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
 
-User.hasMany(Project, {
+//setting type of relationship
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
-
-Project.belongsTo(User, {
+//links parent and child together
+Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Post };
